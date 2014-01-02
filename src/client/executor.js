da83bfe8652ba97aabe188cbc3838dbfc55e717c
Rob Harper <robert.harper@gmail.com>
2014-01-02 13:48:36 -0500

    Use node-csv instead of manual csv writing
    Also sanitize null/false to empty

--- src/client/executor.js
+++ src/client/executor.js
@@ -1,5 +1,5 @@
 var rest = require('restler');
-var fs = require('fs');
+var csv = require('csv');
 var _ = require('lodash');
 var metrics = require('../metrics');
 
@@ -9,51 +9,60 @@ var outputFile = process.argv[3] || 'results.csv'; // 'test.csv';
 var kue = require('kue');
 var jobs = kue.createQueue();
 
+
 var columns = ['url'].concat( _.pluck(metrics, 'name') );
 
-fs.open(outputFile, 'a', function(err, fd){
-  // Write column header
-  fs.write(fd, columns.join(',') + '\n', null, undefined, function (err, written) {
+var output = csv().to.path(outputFile, {
+  flags: 'a',
+  eof: true,
+  columns: columns
+});
 
-    // Start processing jobs...
-    jobs.process('snuffle', function(job, done){
-      
-      console.log('Starting: ' + job.data.url);
+// Write column headers
+output.write(columns);
 
-      rest.get(server, {query: {url: job.data.url}})
-        .on('fail', function(data, response) {
-          console.error('Fail ' + job.data.url + ' - ('+response.statusCode+')');
-          // Allow remote to recover before continuing
-          setTimeout(function() {
-            done(data);
-          }, 5000);
-        })
-        .on('error', function(err, response) {
-          console.error('Error ' + job.data.url + ' - ('+response.statusCode+')', err );
-          done(err);
-        })
-        .on('success', function(result) {
-          // Map results object to value array
-          var line = _.map(columns, function(col) {
-            return JSON.stringify(result[col]);
-          }).join(',');
+// Start processing jobs...
+jobs.process('snuffle', function(job, done){
+  
+  console.log('Starting: ' + job.data.url);
 
-          fs.write(fd, line + '\n', null, undefined, function(err, written) {
-            console.log('Wrote: ' + job.data.url);
-            // Rate limit, delay job completion.
-            setTimeout(function() {
-              done(err);
-            }, 3000);
-          });
-        });
+  rest.get(server, {query: {url: job.data.url}})
+    .on('fail', function(data, response) {
+      console.error('Fail ' + job.data.url + ' - ('+response.statusCode+')');
+      // Allow remote to recover before continuing
+      setTimeout(function() {
+        // 503 indicates app crashed, fail job, allow retry
+        // otherwise likely test page 404'd
+        done(response.statusCode == 503 ? data : null);
+      }, 10000);
+    })
+    .on('error', function(err, response) {
+      console.error('Error ' + job.data.url, err );
+      done(err);
+    })
+    .on('success', function(result) {
+      console.log('Finished: ' + job.data.url);
+
+      // Sanitize for output
+      _.each(result, function(value, key) {
+        if (value == null || value === false) {
+          result[key] = '';
+        }
+      });
+
+      output.write(result);
+
+      // Rate limit, delay job completion.
+      setTimeout(function() {
+        done();
+      }, 3000);
     });
-  });
+});
 
-  process.on('SIGINT', function(){
-    console.log('closing output file...');
-    fs.closeSync(fd);
-    process.exit();
-  });
+process.on('SIGINT', function(){
+  console.log('closing output file...');
+  output.end();
+  process.exit();
 });
 
 
